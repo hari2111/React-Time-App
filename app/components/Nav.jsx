0f12d8c51c11c2059edd293863e4890e874a9733
SRIHARI SARAVANAN <SRIHARI SARAVANAN>
2017-02-01 19:54:10 +0530

    Initial Commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,31 @@
+var React=require('react');
+var {Link,IndexLink}=require('react-router');
+var Nav=()=>{
+  return(
+    <div>
+        <div className="top-bar">
+          <div className="top-bar-left">
+            <ul className="menu">
+              <li className="menu-text">React Time App</li>
+              <li className="menu-text">
+                <IndexLink to='/' activeClassName="active-link">Timer</IndexLink>
+              </li>
+              <li className="menu-text">
+                <Link to='/' activeClassName="active-link">Countdown</Link>
+              </li>
+            </ul>
+          </div>
+            <div className="top-bar-right">
+              <ul className="menu">
+                <li className="menu-text">
+                  Created by hari from mead
+                </li>
+
+              </ul>
+            </div>
+        </div>
+    </div>
+
+  )
+}
+module.exports=Nav;
